ff6682c4cf28c3a6336c450492eb3890f2a5debc
Jerry Bi <jerrybi0522@gmail.com>
2016-04-18 21:14:40 -0400

    uncommenting

--- basic.rb
+++ basic.rb
@@ -1,36 +1,36 @@
-# def murica
-# 	string = gets.chomp
-# 	new_string = string + "Only in America!"
-# 	puts new_string
-# end
+def murica
+	string = gets.chomp
+	new_string = string + "Only in America!"
+	puts new_string
+end
 
-# murica
+murica
 
-# def maximum(array)
-# 	max = array[0]
-# 	array.each do |current|
-# 		if current > max
-# 			max = current
-# 		end
-# 	end
+def maximum(array)
+	max = array[0]
+	array.each do |current|
+		if current > max
+			max = current
+		end
+	end
 
-# 	puts max
-# end
+	puts max
+end
 
-# maximum([1, 2, 3, 2])
+maximum([1, 2, 3, 2])
 
-# def hashing(key, value)
-# 	hash = {}
-# 	i = 0
-# 	while i < key.length
-# 		hash[key[i]] = value[i]
-# 		i += 1
-# 	end
+def hashing(key, value)
+	hash = {}
+	i = 0
+	while i < key.length
+		hash[key[i]] = value[i]
+		i += 1
+	end
 
-# 	puts hash
-# end
+	puts hash
+end
 
-# hashing([:a, :b, :c], [1, 2, 3])
+hashing([:a, :b, :c], [1, 2, 3])
 
 for i in 1..100
 	if i % 3 == 0
